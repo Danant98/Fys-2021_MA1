d332ababbe8251940156160ac085791a4bd85a79
Daniel E. Antonsen <danieleantonsen@gmail.com>
2022-09-25 14:37:47 +0200

    updated main file

---
+++ main.py
@@ -0,0 +1,21 @@
+"""
+Main file for assignment 1 in Fys-2021 Machine Learning. 
+
+@author: Daniel Elisabethsønn Antonsen, UiT Arctic university.
+"""
+# Import libraries and modules
+import numpy as np
+import seaborn as sns
+
+
+class LinearRegression:
+
+    def __init__(self, tset):
+        self.tset = tset
+
+
+
+
+
+if __name__ == '__main__':
+    pass
