1eac3f900f4221ae56b06609da081d8487037800
Daniel E. Antonsen <danieleantonsen@gmail.com>
2022-09-25 21:08:54 +0200

    updated LR file

--- LR.py
+++ LR.py
@@ -9,11 +9,11 @@ class LinearRegression:
     def __init__(self, X:np.ndarray, r:np.ndarray):
         """
         Input:
-            X: np.ndarray shape(n,p-1) p original length of array, datamatrix for traningdata
-            r: np.ndarray shape(n,), continuous data for response
+            X: np.ndarray shape(N,p-1) p original length of array, datamatrix for traningdata
+            r: np.ndarray shape(N,), continuous data for response
         
         Output: 
-            np.ndarray shape(n,), best fit data for inputed data response r
+            rhat: np.ndarray shape(N,), best fit data for inputed data response r
         """
         self._X = np.hstack((np.ones((np.shape(X)[0], 1)), X))
         self._r = r
@@ -34,6 +34,6 @@ class LinearRegression:
             return np.dot(self._X, BP)
         # Caching errors if method does not work
         except Exception as e:
-            print("OPS! Something went wrong. Check {} for details.".format(str(e)))
+            print("OPS! Something went wrong. Check {}\n for details.".format(str(e)))
 
 
