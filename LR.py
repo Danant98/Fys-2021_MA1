3f586a1a514176e16fd006cf745704b51c8687f6
Daniel E. Antonsen <danieleantonsen@gmail.com>
2022-09-25 19:05:47 +0200

    updated LR file

--- LR.py
+++ LR.py
@@ -20,18 +20,21 @@ class LinearRegression:
         self._X = np.hstack((np.ones((np.shape(X)[0], 1)), X))
         self._r = r
 
-    def __bestfit(self):
+    def __bestmodel(self):
         """
-        Method for calculating the best fit/linear regression for r 
+        Method for calculating the best fit model (g(x|theta))
         """
         return np.dot(np.dot(np.linalg.inv(np.dot(self._X.T, self._X)), self._X.T), self._r)
     
     def linear_regression(self):
         """
-        Method for runing the linear regression method
+        Method for calculating the linear regression method
         """
         try:
-            self.__bestfit()
+            # Calling the best model for f(x)
+            BP = self.__bestmodel()
+            return np.dot(self._X, BP)
+        
         except Exception as e:
             print("OPS! Something went wrong. Check {} for details.".format(str(e)))
 
