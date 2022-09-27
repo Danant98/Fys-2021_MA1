aafa732d0bff7c1e4d61460730ab1bf81b238dff
Daniel E. Antonsen <danieleantonsen@gmail.com>
2022-09-27 11:31:11 +0200

    updated files

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ import os
 import pandas as pd
 from LR import LinearRegression
 
-
 # Opening data files and transforming to numpy array
 spotify_data = pd.read_csv(os.path.join("resources", "spotify_data.csv"), delimiter=",").to_numpy()
 R1 = spotify_data[:, 0]
@@ -32,12 +31,14 @@ indexes = np.arange(0, np.shape(spotify_data)[0], 1)
 fig, ax = plt.subplots(1, 2, tight_layout=True)
 ax[0].scatter(indexes, rhat_1, label=r"$\hat{r}$")
 ax[0].scatter(indexes, R1, label=r"$r$")
+ax[0].vlines(indexes, rhat_1, R1, color="green")
 ax[0].set_xlabel("Song by index")
 ax[0].set_ylabel("Popularity of song")
+ax[0].set_title("Popularity of songs by index")
 ax[0].legend(loc="lower left")
 
-ax[1].hist(error_1, bins=indexes)
-
+ax[1].hist(error_1, color="red")
+ax[1].set_title("Error/residuals")
 
 if __name__ == '__main__':
     plt.show()
