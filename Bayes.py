7dc378a0920e9cb06afa0928e03fba3871649f26
Daniel E. Antonsen <danieleantonsen@gmail.com>
2022-09-28 11:52:43 +0200

    added Bayes file

---
+++ Bayes.py
@@ -0,0 +1,15 @@
+"""
+File constaing the Bayes classifier class
+"""
+__author__ = 'Daniel Elisabethsønn Antonsen, UiT Artic university'
+
+# Import libraries and modules
+import numpy as np
+
+
+
+class BayesClassifier:
+
+    def __init__(self):
+        pass
+
